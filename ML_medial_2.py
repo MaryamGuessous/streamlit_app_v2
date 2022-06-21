cc5b72516c4e2da2cd7e81369e46ef60c4440050
GUESSOUS Maryam <Maryam.GUESSOUS@ocpgroup.ma>
2022-06-21 12:25:13 +0100

    initial commit

---
+++ ML_medial_2.py
@@ -0,0 +1,109 @@
+#Step 1: Import the required modules
+import math
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt 
+from sklearn import preprocessing
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import make_classification
+from sklearn.metrics import confusion_matrix
+from sklearn.tree import DecisionTreeClassifier, export_graphviz,DecisionTreeRegressor
+import seaborn as sns
+sns.set(style="white")
+sns.set(style="whitegrid", color_codes=True)
+from sklearn.metrics import accuracy_score
+import seaborn as seabornInstance 
+import statsmodels.api as sm
+import pydot
+from sklearn.linear_model import LinearRegression
+from sklearn import metrics
+
+from sklearn.datasets import make_classification
+from sklearn.metrics import confusion_matrix
+from sklearn.tree import DecisionTreeClassifier, export_graphviz,DecisionTreeRegressor
+
+from sklearn.metrics import mean_squared_error as MSE
+
+from sklearn.tree import DecisionTreeClassifier, export_graphviz,DecisionTreeRegressor
+import graphviz
+
+from sklearn.ensemble import RandomForestRegressor
+
+from sklearn.tree import export_graphviz
+
+import numpy as np
+import pandas as pd
+import random
+import matplotlib.pyplot as plt
+from matplotlib.pylab import rcParams
+
+from numpy import arange
+from pandas import read_csv
+from sklearn.linear_model import RidgeCV
+from sklearn.model_selection import RepeatedKFold
+
+from numpy import mean
+from numpy import std
+from numpy import absolute
+from pandas import read_csv
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import RepeatedKFold
+from sklearn.linear_model import Lasso
+
+from numpy import arange
+from pandas import read_csv
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import RepeatedKFold
+from sklearn.linear_model import Lasso
+
+import warnings
+import pickle
+warnings.filterwarnings("ignore")
+
+
+inputFileName = "donnees_synthese_1606.xlsx"
+data = pd.read_excel(inputFileName)
+
+inputFileName_2 = "donnees_synthese_2_1606.xlsx"
+data_2 = pd.read_excel(inputFileName_2)
+
+data= pd.get_dummies(data, columns=['Sexe'])
+data_2 = pd.get_dummies(data_2, columns=['Sexe'])
+
+data_2= pd.get_dummies(data_2, columns=['Categorie Age'])
+
+data= data.drop([ 'Patient', 'Reference'], axis=1)
+data_2= data_2.drop([ 'Patient', 'Reference'], axis=1)
+
+High_Fibro=data['FibroTest'].mean() + 2.3*data['FibroTest'].std()
+Low_Fibro=data['FibroTest'].mean() - 2.3*data['FibroTest'].std()
+
+High_Acti=data['Actitest'].mean() + 2*data['Actitest'].std()
+Low_Acti=data['Actitest'].mean() - 2*data['Actitest'].std()
+
+data = data[(data['FibroTest']<High_Fibro) & (data['FibroTest']>Low_Fibro)]
+data = data[(data['Actitest']<High_Acti) & (data['Actitest']>Low_Acti)]
+
+
+X= data.drop([ 'FibroTest', 'Actitest'], axis=1)
+Y_F = data[['FibroTest']]
+Y_A = data[['Actitest']]
+
+#Let's divide the datased into training and testing sets
+import random
+random.seed(0)
+X_train_F, X_test_F, y_train_F, y_test_F = train_test_split(X, Y_F, test_size=0.3, random_state=78)
+X_train_A, X_test_A,  y_train_A, y_test_A = train_test_split(X, Y_A, test_size=0.3, random_state=78)
+
+from sklearn.metrics import r2_score
+
+#Regression Linéaire avec la variable Age
+LinReg_F = LinearRegression()
+LinReg_F.fit(X = X_train_F, y = y_train_F)
+
+LinReg_A = LinearRegression()
+LinReg_A.fit(X = X_train_A, y = y_train_A)
+
+pickle.dump(LinReg_F,open('model_F.pkl','wb'))
+pickle.dump(LinReg_A,open('model_A.pkl','wb'))
---
+++ myapp_ML.py
@@ -0,0 +1,84 @@
+import streamlit as st
+import pickle
+import numpy as np
+model_A=pickle.load(open('model_A.pkl','rb'))
+model_F=pickle.load(open('model_F.pkl','rb'))
+
+
+
+def predict_F(Age,Alpha2_Macroglobuline,Haptoglobine,Apolipoproteine_A1, Bilirubine,Gamma_GT,ALAT,Sexe_Femme, Sexe_Homme):
+    input=np.array([[Age,Alpha2_Macroglobuline,Haptoglobine,Apolipoproteine_A1, Bilirubine,Gamma_GT,ALAT,Sexe_Femme, Sexe_Homme]]).astype(np.float64)
+    prediction_F=model_F.predict(input)
+    
+    pred_F='{0:.{1}f}'.format(prediction_F[0][0], 2)
+    
+    return float(pred_F)
+
+def predict_A(Age,Alpha2_Macroglobuline,Haptoglobine,Apolipoproteine_A1, Bilirubine,Gamma_GT,ALAT,Sexe_Femme, Sexe_Homme):
+    input=np.array([[Age,Alpha2_Macroglobuline,Haptoglobine,Apolipoproteine_A1, Bilirubine,Gamma_GT,ALAT,Sexe_Femme, Sexe_Homme]]).astype(np.float64)
+    prediction_A=model_A.predict(input)
+    
+    pred_A='{0:.{1}f}'.format(prediction_A[0][0], 2)
+    
+    return float(pred_A)
+
+def main():
+    st.title("Prédiction médicale")
+    html_temp = """
+    <div style="background-color:#025246 ;padding:10px">
+    <h2 style="color:white;text-align:center;">Actitest, Fibrotest ML App </h2>
+    </div>
+    """
+    st.markdown(html_temp, unsafe_allow_html=True)
+
+    Age = st.text_input("Age",68)
+    Alpha2_Macroglobuline = st.text_input("Alpha2_Macroglobuline",1.94)
+    Haptoglobine = st.text_input("Haptoglobine",1.19)
+    Apolipoproteine_A1 = st.text_input("Apolipoproteine_A1",1.42)
+    Bilirubine = st.text_input("Bilirubine",5)
+    Gamma_GT = st.text_input("Gamma_GT",22)
+    ALAT = st.text_input("ALAT",11)
+    Sexe_Femme = st.text_input("Sexe_Femme",0)
+    Sexe_Homme = st.text_input("Sexe_Homme",1)
+
+
+    A0_html="""  
+      <div style="background-color:#F4D03F;padding:10px >
+       <h2 style="color:white;text-align:center;"> A0</h2>
+       </div>
+    """
+    A1_html="""  
+      <div style="background-color:#F08080;padding:10px >
+       <h2 style="color:black ;text-align:center;"> A1</h2>
+       </div>
+    """
+    A2_html="""  
+      <div style="background-color:#F08080;padding:10px >
+       <h2 style="color:black ;text-align:center;"> A2</h2>
+       </div>
+    """
+
+    A3_html="""  
+      <div style="background-color:#F08080;padding:10px >
+       <h2 style="color:black ;text-align:center;"> A3</h2>
+       </div>
+    """
+
+    if st.button("Predict"):
+        output_A=predict_A(Age,Alpha2_Macroglobuline,Haptoglobine,Apolipoproteine_A1, Bilirubine,Gamma_GT,ALAT,Sexe_Femme, Sexe_Homme)
+        st.success('Actitest value {}'.format(output_A))
+        output_F=predict_F(Age,Alpha2_Macroglobuline,Haptoglobine,Apolipoproteine_A1, Bilirubine,Gamma_GT,ALAT,Sexe_Femme, Sexe_Homme)
+        st.success('Fibrotest value {}'.format(output_F))
+
+        if output_A < 0.3:
+            st.markdown(A0_html,unsafe_allow_html=True)
+        elif output_A < 0.51 :
+            st.markdown(A1_html,unsafe_allow_html=True)
+        elif output_A < 0.625 :
+            st.markdown(A2_html,unsafe_allow_html=True)
+        else: 
+            st.markdown(A3_html,unsafe_allow_html=True)
+
+
+if __name__=='__main__':
+    main()
